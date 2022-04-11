f6634e85c244ee27853f3693de1976a804b72433
delaquash <olaide1191@gmail.com>
2022-04-11 15:30:34 +0100

    implemented API request from rapid api

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import millify from 'millify';
+import { Col, Row, Statistic, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
- const Homepage =()=>{
+const { Title } = Typography;
+
+ const Homepage = () =>{
+   const { data, isFetching } = useGetCryptosQuery();
+    console.log(data);
+
   return (
-    <div>Emmanuel</div>
+    <>
+      <Typography level={2} className='heading'>
+            Global Crypto Stats
+      </Typography>
+      <Row>
+        <Col span={12}><Statistic Title="Total Cryptocurrencies" value="5" />Cryptocurrencies</Col>
+        <Col span={12}><Statistic Title="Total Exchanges" value="5" />Exchanges</Col>
+        <Col span={12}><Statistic Title="Total Market Cap" value="5" />Market Cap</Col>
+        <Col span={12}><Statistic Title="Total Markets" value="5" />Markets</Col>
+        <Col span={12}><Statistic Title="Total 24h Volume" value="5" />24h Volume</Col>
+      </Row>
+    </>
+
   )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'b286648626mshb5a701605ade81bp154604jsncf5b4b6ff8f6'
+}
+
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi ({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
