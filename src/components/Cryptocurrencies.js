4db0e1d2a4fd850a11addbd6139929ac9f15c1f2
delaquash <olaide1191@gmail.com>
2022-04-12 04:22:30 +0100

    implemented API call for cryptocurrencies component

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Card, Col, Input, Row } from 'antd';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
- const Cryptocurrencies =()=>{
-   const { data:cryptosList, isFetching } = useGetCryptosQuery()
+ const Cryptocurrencies =({ simplified })=>{
+   const count = simplified ? 10 : 100;
+  //  renamed data to cryptoList
+   const { data:cryptoList, isFetching } = useGetCryptosQuery(count);
+   const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+    if(isFetching) return 'Loading....'
   return (
-    <div>Emmanuel</div>
+    <>
+      <Row gutter={[ 32, 32]} className="crypto-card-container">
+        {
+          cryptos?.map((currency) => (
+            <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+              <Link to={`/crypto/${currency.id}`}>
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={<img className='crypto-image' src={currency.iconUrl} alt="Coin"/>}
+                  hoverable
+                >
+                  <p>Price:{millify(currency.price)}</p>
+                  <p>Market Cap:{millify(currency.marketCap)}</p>
+                  <p>Daily Changes:{millify(currency.change)}%</p>
+
+                </Card>
+              </Link>
+            </Col>
+          ))
+        }
+    </Row>
+    </>
+  
   )
 }
 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,14 +2,13 @@ import React from 'react';
 import millify from 'millify';
 import { Col, Row, Statistic, Typography } from 'antd';
 import { Link } from 'react-router-dom';
-
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
  const Homepage = () =>{
-   const { data, isFetching } = useGetCryptosQuery();
+   const { data, isFetching } = useGetCryptosQuery(10);
    const globalStats = data?.data?.stats;
     if(isFetching) return 'Loading....';
 
