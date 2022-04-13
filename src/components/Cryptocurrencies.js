c44d07bf07e013d653d0141a60f1841354b27f6d
delaquash <olaide1191@gmail.com>
2022-04-13 09:41:10 +0100

    implemented search in cryptocurrencies homepage

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Col, Input, Row } from 'antd';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
@@ -6,17 +6,30 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
  const Cryptocurrencies =({ simplified })=>{
-   const count = simplified ? 10 : 100;
+   const count = simplified ? 100 : 100;
   //  renamed data to cryptoList
    const { data:cryptoList, isFetching } = useGetCryptosQuery(count);
-   const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+   const [ cryptos, setCryptos ] = useState([]);
+const [searchItem, setSearchItem] = useState('')
+
+    
+
+    useEffect(() => {
+      const filteredCrypto = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchItem.toLowerCase()));
+      setCryptos(filteredCrypto)
+     
+    }, [cryptoList, searchItem])
+    
     if(isFetching) return 'Loading....'
   return (
     <>
+    <div className="search-crypto">
+        <Input type="text" placeholder='Search Cryptocurrency' onChange={(e) =>setSearchItem(e.target.value) } />
+    </div>
       <Row gutter={[ 32, 32]} className="crypto-card-container">
         {
           cryptos?.map((currency) => (
-            <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+            <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
               <Link to={`/crypto/${currency.id}`}>
                 <Card
                   title={`${currency.rank}. ${currency.name}`}
@@ -26,7 +39,6 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
                   <p>Price:{millify(currency.price)}</p>
                   <p>Market Cap:{millify(currency.marketCap)}</p>
                   <p>Daily Changes:{millify(currency.change)}%</p>
-
                 </Card>
               </Link>
             </Col>
@@ -34,7 +46,6 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
         }
     </Row>
     </>
-  
   )
 }
 
